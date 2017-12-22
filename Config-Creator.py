ed4758ed44235a73e58cf709924de653d87cb62f
routeallthings <30909318+routeallthings@users.noreply.github.com>
2017-12-22 11:18:14 -0600

    Initial import

---
+++ Config-Creator.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+'''
+---AUTHOR---
+Name: Matt Cross
+Email: routeallthings@gmail.com
+
+---PREREQ---
+Install GIT (most likely you do not have the XLHELPER module. This has been forked to make it work with Python 2.7. It will ask you to download automatically)
+Install OpenPYXL
+Install FileInput
+
+---VERSION---
+VERSION 1.0
+Currently Implemented Features
+-Text based variable replacement
+-XLSX based variable replacement
+
+
+Features planned in the near future
+
+
+'''
+
+'''IMPORT MODULES'''
+import os
+import sys
+#
+try:
+	from openpyxl import load_workbook
+except ImportError:
+	requestsinstallstatus = fullpath = raw_input ('openpyxl module is missing, would you like to automatically install? (Y/N): ')
+	if 'Y' in requestsinstallstatus or 'y' in requestsinstallstatus or 'yes' in requestsinstallstatus or 'Yes' in requestsinstallstatus or 'YES' in requestsinstallstatus:
+		os.system('python -m pip install openpyxl')
+		from openpyxl import load_workbook
+	else:
+		print 'You selected an option other than yes. Please be aware that this script requires the use of Pandas. Please install manually and retry'
+		sys.exit()
+#
+try:
+	import fileinput
+except ImportError:
+	requestsinstallstatus = fullpath = raw_input ('FileInput module is missing, would you like to automatically install? (Y/N): ')
+	if 'Y' in requestsinstallstatus or 'y' in requestsinstallstatus or 'yes' in requestsinstallstatus or 'Yes' in requestsinstallstatus or 'YES' in requestsinstallstatus:
+		os.system('python -m pip install FileInput')
+		import FileInput
+	else:
+		print 'You selected an option other than yes. Please be aware that this script requires the use of FileInput. Please install manually and retry'
+		sys.exit()
+#	
+# Darth-Veitcher Module https://github.com/darth-veitcher/xlhelper
+#
+from pprint import pprint
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter, column_index_from_string
+from collections import OrderedDict
+try:
+	import xlhelper
+except ImportError:
+	requestsinstallstatus = fullpath = raw_input ('xlhelper module is missing, would you like to automatically install? (Y/N): ')
+	if 'Y' in requestsinstallstatus or 'y' in requestsinstallstatus or 'yes' in requestsinstallstatus or 'Yes' in requestsinstallstatus or 'YES' in requestsinstallstatus:
+		os.system('python -m pip install git+git://github.com/routeallthings/xlhelper.git')
+		import xlhelper
+	else:
+		print 'You selected an option other than yes. Please be aware that this script requires the use of xlhelper. Please install manually and retry'
+		sys.exit()
+
+# Netmask Function
+from socket import inet_ntoa
+from struct import pack
+
+def calcDottedNetmask(mask):
+    bits = 0xffffffff ^ (1 << 32 - mask) - 1
+    return inet_ntoa(pack('>I', bits))
+
+
+	
+	
+
+
+# Import Excel File Method
+XLSimportq = raw_input('Import XLS file for data entry (Y/N)? ')
+if 'y' in XLSimportq.lower():
+	excelfilelocation = raw_input('File to load the excel data from (e.g. C:/Python27/exceldata.xlsx):')
+	if excelfilelocation == '':
+		excelfilelocation = 'C:/Python27/exceldata.xlsx'
+
+# Import TXT/CLI Method
+if 'n' in XLSimportq.lower():
+	templatefilelocation = raw_input('File to load the default template from (e.g. C:/Python27/basetemplate.txt):')
+	if templatefilelocation == '':
+		templatefilelocation = 'C:/Python27/basetemplate.txt'
+	try:
+		with open(templatefilelocation, 'r') as file :
+			configfiledata = file.read()
+	except:
+		print 'Error in loading the base template. Please enter a good path for the file.'
+	# Import Variable List
+	variablefilelocation = raw_input("File to load variables from (e.g. C:/Python27/variables.txt):")
+	if variablefilelocation == '':
+		variablefilelocation = 'C:/Python27/variables.txt'
+	try:
+		with open(variablefilelocation) as f:
+			variablelist = f.read().splitlines() 
+	except:
+		print 'Error in loading the variable list file. Please enter a good path for the file.'
+	finalconfigpath = 'C:/Python27/' + excelmatchq2 + '.txt'
+	finalconfigfile = raw_input('Configuration file path to save to (e.g. ' + finalconfigpath + '):')
+	if finalconfigfile == '':
+		finalconfigfile = finalconfigpath
+
+# Save File
+
+
+if 'n' in XLSimportq.lower():
+# Find and Replace
+	for variable in variablelist:
+		if not '#NOTE:' in variable:
+			variablename = variable.rsplit(':',1)[1]
+			variablename = variablename.strip()
+			variableq = 'Please enter the value for the following variable ' + variablename + ' :'
+			variablea = raw_input(variableq)
+			configfiledata = configfiledata.replace(variablename,variablea)
+if 'y' in XLSimportq.lower():
+	for hosts in xlhelper.sheet_to_dict(excelfilelocation,'Name-Template'):
+		OutputPath = hosts.get('Config Output').encode('utf-8')
+		Hostname = hosts.get('Hostname').encode('utf-8')
+		TemplatePath = hosts.get('Template Location').encode('utf-8')
+		OutputSave = OutputPath + '\\' + Hostname + '.txt'
+		try:
+			with open(TemplatePath, 'r') as file :
+				configfiledata = file.read()
+		except:
+			print 'Error in loading the base template. Please enter a good path for the file.'
+		for variables in xlhelper.sheet_to_dict(excelfilelocation,'Variables'):
+			xlscolumnname = variables.get('Variable Name').encode('utf-8')
+			#
+			for values in xlhelper.sheet_to_dict(excelfilelocation,'Values'):
+				matchvalue = values.get('#HOSTNAME#').encode('utf-8')
+				if matchvalue == Hostname:
+					try:
+						variableresult = values.get(xlscolumnname).encode('utf-8')
+					except:
+						variableresult = values.get(xlscolumnname)
+						variableresult = str(variableresult)
+					if "/" in variableresult:
+						variableresult = variableresult.split('/')
+						cidrvariable = variableresult[1]
+						cidrvariable = int(cidrvariable)
+						subnetmask = calcDottedNetmask(cidrvariable)
+						networkaddress = variableresult[0]
+						networkaddress = networkaddress.split('.')
+						firstoctet = networkaddress[0]
+						secondoctet = networkaddress[1]
+						thirdoctet = networkaddress[2]
+						lastoctet = networkaddress[3]
+						lastoctet = int(lastoctet)
+						lastoctet = lastoctet + 1
+						lastoctet = str(lastoctet)
+						gatewayip = firstoctet + '.' + secondoctet + '.' + thirdoctet + '.' + lastoctet
+						variableresult = str(gatewayip) + ' ' + str(subnetmask)
+					variablereplace = variableresult
+			configfiledata = configfiledata.replace(xlscolumnname,variablereplace)
+			variablereplace = ''
+			xlscolumnname = ''
+			setname = ''
+		print 'Writing config file for ' + Hostname
+		with open(OutputSave, 'w') as file:
+			file.write(configfiledata)
+		print 'Done...'	
+if 'n' in XLSimportq.lower():
+	print 'Script nearing completion. Saving final configuration file.'
+	with open(finalconfigfile, 'w') as file:
+		file.write(configfiledata)
+	print 'Done...'
+# Exit
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Config-Creator
+# Config Creator
+
+The goal of this script was to be able to take XLSX data and mass create switch configurations based on the data in the XLSX spreadsheet. I also added in a option for TXT input for one off tasks.
+
+## Getting Started
+
+Look at the example files. In the XLSX spreadsheet, you can add as many columns as you want to the values page, just make sure that the column name is in the variable so it knows to pull data.
+
+Step 1. Build the template. Any data you want to pull from XLSX you can reference with a variable like #VARIABLE1#.
+Step 2. Build the XLSX spreadsheet. Do not change the header information in the first 2 pages. Only change header information based on the variable name in the values page.
+Step 3. Run and profit
+
+Report any issues to my email and I will get them fixed.
+
+### Prerequisites
+
+GIT (This is required to download the XLHELPER module using a fork that  I made for compatibility with Python 2.7)
+XLHELPER
+OPENPYXL
+
+## Deployment
+
+Just execute the script and answer the questions
+
+## Features
+- Text-based variable replacement
+- XLSX-based variable replacement
+- Mass produce configurations based on template and data in XLSX spreadsheet
+
+## *Caveats
+- None
+
+## Versioning
+
+Version 1.0 - Created initial copy of tool
+
+## Authors
+
+* **Matt Cross** - [RouteAllThings](https://github.com/routeallthings)
+
+See also the list of [contributors](https://github.com/routeallthings/Config-Creator/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the GNU - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Thanks to HBS for giving me a reason to write this.
